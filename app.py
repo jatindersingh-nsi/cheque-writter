92d803ac088f5f005be5253d4f0df3814779f768
jatinder.singh <jatinder.singh@netsolutions.com>
2021-01-28 13:04:27 +0530

    resolve conflict:

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import time
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from num2words import num2words
 import decimal
 
@@ -17,12 +17,25 @@ def create_cheque():
     in_words = request.form['amount']
     try:
         in_words = num2words(in_words).replace('-', ' ')
-        #in_words = in_words.title()
+        in_words = in_words.title()
     except (ValueError, decimal.InvalidOperation):
         return render_template('error.html')
     return render_template('cheque.html', result={'amount': request.form['amount'], 'in_words': in_words})
 
 
+@app.route('/list', methods=['GET'])
+def list_cheques():
+    cheques = [{
+        'name': 'Mark',
+        'amount': 200
+    },
+    {
+        'name': 'John',
+        'amount': 123.9
+    }]
+    return jsonify(cheques)
+
+
 if __name__ == "__main__":
-    time.sleep(21)
+    time.sleep(5)
     app.run(host="0.0.0.0", port=5001, debug=False)
